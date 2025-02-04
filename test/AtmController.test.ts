c481ccca4a5052bfd299cfde91711bc1837a6911
Yoonseo Han <yooncer00@gmail.com>
2025-02-04 20:34:42 +0900

    test: add test code for balance, withdraw, deposit activity

--- test/AtmController.test.ts
+++ test/AtmController.test.ts
@@ -23,6 +23,32 @@ describe ('ATM Controller', () => {
     atm.validatePin('1234');
 
     expect(atm.selectAccount('1')).toBe(true);
-    expect(atm.selectAccount('invalid')).toBe(false);
+    expect(atm.selectAccount('Invalid Account')).toBe(false);
+  });
+
+  test('Should perform balance check correctly', async () => {
+    atm.insertCard('1234-5678');
+    atm.validatePin('1234');
+    atm.selectAccount('1');
+    
+    expect(atm.checkBalance()).toBe(500);
+  });
+
+  test('Should perform withdrawal correctly', async () => {
+    atm.insertCard('1234-5678');
+    atm.validatePin('1234');
+    atm.selectAccount('1');
+    
+    expect(atm.withdraw(500)).toBe(true);
+    expect(atm.checkBalance()).toBe(0);
+  });
+
+  test('Should perform deposit correctly', async () => {
+    atm.insertCard('1234-5678');
+    atm.validatePin('1234');
+    atm.selectAccount('1');
+    
+    expect(atm.deposit(500)).toBe(true);
+    expect(atm.checkBalance()).toBe(1000);
   });
 });
